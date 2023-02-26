73cd1f443fd50002234180899ebd25c7eb67d70f
UtkarshDayal <udayal@solutechnology.com>
2023-02-25 22:36:01 -0500

    Added even more things to the character sheet

--- app/models/char_sheet.rb
+++ app/models/char_sheet.rb
@@ -4,7 +4,7 @@ class CharSheet
   field :player_name, type: String
   field :char_name, type: String
   field :race, type: String
-  field :class, type: String
+  field :character_class, type: String
   field :level, type: Integer
   field :background, type: String
   field :proficiency_bonus, type: Integer
@@ -19,4 +19,21 @@ class CharSheet
   field :intelligence, type: Integer
   field :wisdom, type: Integer
   field :charisma, type: Integer
+  field :passive_perception, type: Integer
+  field :speed, type: Integer
+  field :hit_dice, type: Integer
+  field :death_save_success, type: Integer
+  field :death_save_failure, type: Integer
+  field :weapons, type: String
+  field :equipment, type: String
+  field :personality, type: String
+  field :ideals, type: String
+  field :bonds, type: String
+  field :flaws, type: String
+  field :features, type: String
+  field :gold, type: Integer
+  field :silver, type: Integer
+  field :copper, type: Integer
+  field :platinum, type: Integer
+  field :spells, type: String
 end
--- char_sheet_scaffolds.bat
+++ char_sheet_scaffolds.bat
@@ -1,5 +1,10 @@
 rails g scaffold CharSheet player_name:string char_name:string^
-  race:string class:string level:integer background:string proficiency_bonus:integer^
+  race:string character_class:string level:integer background:string proficiency_bonus:integer^
      max_hp:integer current_hp:integer temp_hp:integer armour_class:integer initiative:integer^
      strength:integer dexterity:integer constitution:integer intelligence:integer wisdom:integer charisma:integer^
-      --force
+      passive_perception:integer speed:integer hit_dice:integer death_save_success:integer death_save_failure:integer ^
+       weapons:text equipment:text personality:text ideals:text bonds:text flaws:text features:text gold:integer^
+        silver:integer copper:integer platinum:integer spells:text --force
+@REM   TODO figure out how to represent the following:
+@REM     - proficiencies in skills/saving throws
+@REM     - spells
